1f09f286935ffefbe8d8aadba10f827f3b1b11f4
arthurvanl <arthurvanleeuwen@hotmail.nl>
2022-06-15 10:10:51 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -5,29 +5,31 @@ try {
 
     /* SSH input commands */
     const commands = core.getInput('commands');
-    if(commands){
-        console.log(commands)
-        let items = commands.split("\n");
-        console.log(items)
-    } else {
-        const host = {
-            server: {
-                host: core.getInput('host'),
-                userName: core.getInput('username'),
-                password: core.getInput('password')
-            },
-            commands: [ssh_1, ssh_2, ssh_3, ssh_4]
-        }
-        /* creating new SSH client */
-        const SSH = new SSH2Shell(host);
-        
-        /* Connecting... */
-        SSH.connect(function(sessionText){
-            /* Response text */
-            console.log(sessionText);
-            console.log("Successfully executed ssh commands!");
-        });
+
+    /* Splitting commands if there are multiple */
+    let items = commands.split("\n");
+
+    const host = {
+        server: {
+            host: core.getInput('host'),
+            userName: core.getInput('username'),
+            password: core.getInput('password')
+        },
+        commands: [items],
     }
+
+    /* creating new SSH client */
+    const SSH = new SSH2Shell(host);
+    
+    /* Connecting... */
+    SSH.connect(function(sessionText){
+        /* Response text */
+        console.log(sessionText);
+        if(sessionText == 'Ready'){
+            console.log("Successfully executed ssh commands!");
+        }
+    });
+
 } catch (error) {
     core.setFailed(error.message);
 }
